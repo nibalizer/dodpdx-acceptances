d674d14dcc4e07be110e898c381d1e27bd1dfc13
Spencer Krum <nibz@spencerkrum.com>
2016-06-07 15:46:28 -0700

    Improve script, filter out ignites

--- accptances.py
+++ accptances.py
@@ -10,16 +10,18 @@ from jinja2 import Environment, FileSystemLoader
 def load_csv(filename='accepted.csv'):
     print "Whoo loading data from file {0}:".format(filename)
     with open(filename, 'rb') as csvfile:
-        csvdata = csv.reader(csvfile, delimiter=',')
+        reader = csv.DictReader(csvfile)
         talks = []
-        for row in csvdata:
+        for row in reader:
             data = {}
-            data['speaker'] = row[24] + ' ' + row[25]
-            data['title'] = row[1]
-            data['bio'] = row[29]
-            data['description'] = row[2]
+            data['speaker'] = row['speaker 1: first_name']
+            data['title'] = row['title']
+            data['bio'] = row['speaker 1: bio']
+            data['description'] = row['description']
+            data['type'] = row['What length of talk is this?']
             talks.append(data)
         return talks
+
 
 
 if __name__ == "__main__":
@@ -29,8 +31,9 @@ if __name__ == "__main__":
 
     data = load_csv()
     for talk in data:
-        print template.render(talk=talk)
-        print
-        print
-        print '#'
-        print
+        if talk['type'] == '30 minute':
+            print template.render(talk=talk)
+            print
+            print
+            print '#'
+            print
